46439b2411304ac8605e1258c73ce9f10e3ede2c
Karim Oulad Chalha <herr.linux88@gmail.com>
2015-03-02 13:14:20 +0000

    New rewriting portfilio with MaterializeCSS

--- js/ngDirective.js
+++
@@ -1,7 +0,0 @@
-app.directive('aside', function(){
-	// Runs during compile
-	return {
-		restrict: 'A',
-		templateUrl: 'views/aside.html'
-	};
-});
--- js/ngFactory.js
+++
@@ -1,7 +0,0 @@
-app.factory('postsFactory', ['$http', function($http){
-	return {
-		getList: function(func){
-			$http.get('db/posts.json').success(func);
-		}
-	};
-}])
