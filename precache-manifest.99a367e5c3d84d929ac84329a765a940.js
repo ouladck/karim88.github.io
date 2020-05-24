6b9cbe49ea71876ff9f3b82a90cd76d5175a9d1c
karim Oulad Chalha <herr.linux88@gmail.com>
2020-05-24 11:14:40 +0000

    Updates

---
+++ precache-manifest.99a367e5c3d84d929ac84329a765a940.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf579ab1457a08bcd69196979a8257c",
+    "url": "/static/media/mrcmaroc.cdf579ab.png"
+  },
+  {
+    "revision": "2e90d7dbb8f8f33ab831",
+    "url": "/static/css/main.c34bcb15.chunk.css"
+  },
+  {
+    "revision": "a9c22585a9b6ac52dee7",
+    "url": "/static/js/1.a9c22585.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b27e3e07c0320b2e34ae70448d0c75e6",
+    "url": "/static/media/intro-layer.b27e3e07.svg"
+  },
+  {
+    "revision": "7f9e9b1529d66adcfe94e29927155528",
+    "url": "/static/media/zayousa.7f9e9b15.png"
+  },
+  {
+    "revision": "26c48255e8b96449cbd2b7ee84bf15f2",
+    "url": "/static/media/falak.26c48255.png"
+  },
+  {
+    "revision": "2e90d7dbb8f8f33ab831",
+    "url": "/static/js/main.2e90d7db.chunk.js"
+  },
+  {
+    "revision": "1cbb7caea85d422ae4aaa34487ee50ea",
+    "url": "/static/media/planksetting.1cbb7cae.gif"
+  },
+  {
+    "revision": "6906543de7d47d40496b8f50837885c7",
+    "url": "/static/media/demo.6906543d.gif"
+  },
+  {
+    "revision": "e2f0fc35b45657460ffe10328b76d002",
+    "url": "/static/media/desktop.e2f0fc35.jpg"
+  },
+  {
+    "revision": "0a796852e4bca0d9212015346e8d08eb",
+    "url": "/static/media/karim.0a796852.jpg"
+  },
+  {
+    "revision": "d6b55fe558611a8d22ca91878cd66e16",
+    "url": "/static/media/sale.d6b55fe5.jpg"
+  },
+  {
+    "revision": "7331c2f10cd0092c4f2e5ebf106de524",
+    "url": "/index.html"
+  }
+];
